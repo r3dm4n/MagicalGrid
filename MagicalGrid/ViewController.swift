1ab127cf3fffaf361855ad9cfebbe19a62cb5878
Alexandru Corut <r3dm4n@me.com>
2017-04-14 15:27:06 +0300

    MagicalGrid

--- MagicalGrid/ViewController.swift
+++ MagicalGrid/ViewController.swift
@@ -12,14 +12,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        let numViewPerRow = 15
+        let width = view.frame.width / CGFloat(numViewPerRow)
+        
+        for j in 0...30 {
+            for i in 0...numViewPerRow {
+                let cellView = UIView()
+                cellView.backgroundColor = randomColor()
+                cellView.frame = CGRect(x: CGFloat(i) * width, y: CGFloat(j) * width, width: width, height: width)
+                cellView.layer.borderWidth = 0.5
+                cellView.layer.borderColor = UIColor.black.cgColor
+                view.addSubview(cellView)
+            }
+        }
+        
+        view.addGestureRecognizer(UIPanGestureRecognizer(target:self, action: #selector(handlePan)))
+        
+        }
+    
+    func handlePan(gesture: UIPanGestureRecognizer) {
+        let location = gesture.location(in: view)
+        print(location)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    fileprivate func randomColor() -> UIColor {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+      
+        
     }
-
-
 }
 
